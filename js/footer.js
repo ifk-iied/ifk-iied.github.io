9ddcdefc4b3474a8a89deb7eaaa6080ade90010d
AneesMuzaffar <anees.muzaffar000@gmail.com>
2017-10-19 17:06:22 +0530

    Fixed, updated and added content

---
+++ js/footer.js
@@ -0,0 +1,3 @@
+function footer(){
+     document.write('&copy; '+new Date().getFullYear()+'. All rights reserved | Designed by <a href="http://alphamstudios.com" target="_blank"> Alpha Magnus Studios</a>') ;
+}
