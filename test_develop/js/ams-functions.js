2311698340cdbe564db08dad59e18023f323ecd7
Afaan Bilal <AfaanBilal@users.noreply.github.com>
2017-10-30 20:15:40 +0530

    Update ams-functions.js

--- test_develop/js/ams-functions.js
+++ test_develop/js/ams-functions.js
@@ -1,5 +1,49 @@
-function header() {
-     document.write();
+function header_top() {
+     document.write(`<!-- header-top -->
+	<div class="header-top">
+		<!-- container -->
+		<div class="container">
+			<div class="social w3-agileitsicons">
+				<ul>
+					<li>
+						<a href="#">
+							<i class="fa fa-facebook" aria-hidden="true"></i>
+						</a>
+					</li>
+					<li>
+						<a class="twt" href="#">
+							<i class="fa fa-twitter" aria-hidden="true"></i>
+						</a>
+					</li>
+					<li>
+						<a class="drbl" href="#">
+							<i class="fa fa-linkedin" aria-hidden="true"></i>
+						</a>
+					</li>
+					<li>
+						<a class="be" href="#">
+							<i class="fa fa-dribbble" aria-hidden="true"></i>
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div class="w3layouts-details">
+				<ul>
+					<li>
+						<a href="#">
+							<span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>
+                                   ` + email("iforkashmir", "gmail", "com") + `
+                              </a>
+					</li>
+					<li>
+						<span class="glyphicon glyphicon-earphone" aria-hidden="true"></span>(+1) 000 123 456789</li>
+				</ul>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+		<!-- //container -->
+	</div>
+	<!-- //header-top -->`);
 }
 
 function footer() {
