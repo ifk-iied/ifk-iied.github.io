8dfe13420bc3d966f0ad6539e21b3f6a68980cb7
Afaan Bilal <AfaanBilal@users.noreply.github.com>
2017-10-30 20:06:04 +0530

    Update and rename footer.js to ams-functions.js

--- test_develop/js/ams-functions.js
+++ test_develop/js/ams-functions.js
@@ -1,3 +1,7 @@
-function footer(){
+function header() {
+     document.write();
+}
+
+function footer() {
      document.write('&copy; ' + new Date().getFullYear() + ' IFK. All Rights Reserved. | Designed by <a href="https://www.alphamstudios.com" target="_blank">Alpha Magnus Studios</a>') ;
 }
