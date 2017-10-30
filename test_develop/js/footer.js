26b1b01c512d2bb9bfb8b634d10bed526e1d7d9c
Afaan Bilal <AfaanBilal@users.noreply.github.com>
2017-10-30 20:03:18 +0530

    Update footer.js

--- test_develop/js/footer.js
+++ test_develop/js/footer.js
@@ -1,3 +1,3 @@
 function footer(){
-     document.write('&copy; ' + new Date().getFullYear() + ' IFK. All rights reserved | Designed by <a href="https://www.alphamstudios.com" target="_blank">Alpha Magnus Studios</a>') ;
+     document.write('&copy; ' + new Date().getFullYear() + ' IFK. All Rights Reserved. | Designed by <a href="https://www.alphamstudios.com" target="_blank">Alpha Magnus Studios</a>') ;
 }
